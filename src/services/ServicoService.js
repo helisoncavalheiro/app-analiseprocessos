4af086eea6de976e1e41f2d462f26800e40f523f
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-05-23 16:19:31 -0300

    Adiciona uma função para acessar o endpoint `/servico/{id}`
    Esta adição é necessária para recuperar um determinado serviço da API.
    
    Problemas:
    
        *
    
    [Ticket: HIS004]

--- src/services/ServicoService.js
+++ src/services/ServicoService.js
@@ -24,4 +24,24 @@ function getServicos() {
     });
 }
 
-export { getServicos }
+function getServico(id) {
+    return new Promise((resolve, reject) => {
+
+        Api.get(`/servico/${id}`)
+            .then(result => {
+                resolve({
+                    code: result.status,
+                    msg: result.statusText,
+                    data: result.data
+                })
+            })
+            .catch(error => {
+                reject({
+                    code: error.response.status,
+                    msg: error
+                });
+            })
+    });
+}
+
+export { getServicos, getServico }
