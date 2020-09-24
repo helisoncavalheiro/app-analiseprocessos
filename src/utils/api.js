c6d1ea6917080aaee5cef998cdc45cccbebdec2e
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-09-24 20:45:01 -0300

    Refatora a conexão com a API.
    Foi adicionado à configuração da API um header de autorização com o token.
    
    [Ticket: HIS006]

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
 const url = process.env.REACT_APP_ANALISEPROCESSOS_API_HOST;
+const token = sessionStorage.getItem('token');
 
 const Api = axios.create({
     baseURL: url,
+    headers:{
+        'Authorization': `Bearer ${token}`
+    }
+
 });
 
-export default Api ;
+
+export default Api;
