fca4d670141c61675929de844f1376a40d341be1
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-09-24 20:40:23 -0300

    Refatora o componente App.
    Foi reativado o componente e adicionado o formulário de login caso não haja usuário autenticado.
    
    [Ticket: HIS006]

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Sidebar from './components/menu/Sidebar';
 import Header from './components/Header';
 import Content from './components/Content';
 import './css/sb-admin/style.css';
 import './css/style.css';
+import Login from './components/screens/Login';
+import { isAuthenticated } from './services/LoginService.js';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      isAuthenticaded: isAuthenticated()
+    }
+
+    this.handleAuthChange = this.handleAuthChange.bind(this);
   }
+
+  handleAuthChange() {
+    this.setState({
+      isAuthenticaded: isAuthenticated()
+    });
+  }
+
   render() {
     return (
       <div>
-        {/*
-      <BrowserRouter>
-        <div id="wrapper" className="sb-nav-fixed">
-          <Header />
-          <div id="layoutSidenav">
-            <Sidebar />
-            <Content />
-          </div>
-        </div>
-      </BrowserRouter>
-      */
-        }
+        <BrowserRouter>
+          {this.state.isAuthenticaded ? (
+
+            <div id="wrapper" className="sb-nav-fixed">
+              <Header />
+              <div id="layoutSidenav">
+                <Sidebar />
+                <Content />
+              </div>
+            </div>
+
+          )
+            : (
+              <div className="row">
+                <div className="col-md-4 col-sm-12 offset-md-4">
+                  <Login onLogin={this.handleAuthChange} />
+                </div>
+              </div>
+            )}
+        </BrowserRouter >
       </div>
     );
   }
