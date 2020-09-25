0d2b678eb8f8e38f49479fcc9d045a1cc0a032f4
Helison Ortiz <helisson.cavalheiro@gmail.com>
2020-09-25 00:18:22 -0300

    Refatora o componente de Login.
    Foram feitas melhorias no design.
    
        Problemas:
    
            * Não foi possível adicionar o CSS do template SB Admin 2.
    
    [Ticket: HIS006]

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Card, Form, Button, Alert } from 'react-bootstrap';
+import { Card, Form, Button, Alert, Container, Row } from 'react-bootstrap';
 import { login } from '../../services/LoginService.js';
 import Loader from '../Loader.js';
+//import "../../css/sb-admin-2/sb-admin-2.min.css";
 
 export default class Login extends React.Component {
 
@@ -23,7 +24,7 @@ export default class Login extends React.Component {
         this.setState({ loading: true });
         this.login(this.state.user, this.state.password)
             .then(res => {
-                
+
                 if (res.statusCode >= 200 && res.statusCode <= 300) {
                     sessionStorage.setItem("token", res.data.token);
                     this.setState({ loading: false });
@@ -37,44 +38,52 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <Card>
-                    <Card.Header>
-                        <div className="row">
-                            <div className="col-sm-6 offset-sm-3">
-                                <h4>Login</h4>
-                            </div>
+            <div className="bg-gradient-primary" style={{position: 'absolute', width: '100%', height: '100%'}}>
+                <Container>
+                    <Row className="justify-content-center">
+                        <div class="col-sm-12 col-md-6">
+
+                            <Card className="o-hidden border-0 shadow-lg my-5">
+                                <Card.Body className="p-0">
+                                    <Row>
+                                        <div class="col-lg-12">
+                                            <div className="p-5">
+                                                <div class="text-center">
+                                                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                                </div>
+                                                <Form className="user" onSubmit={this.handleLogin}>
+                                                    <Form.Group controlId="login">
+                                                        <Form.Control className="form-control-user" type="text" placeholder="Digite seu login" onChange={evt => this.setState({ user: evt.target.value })} />
+                                                    </Form.Group>
+
+                                                    <Form.Group controlId="senha">
+                                                        <Form.Control className="form-control-user" type="password" placeholder="Digite sua senha" onChange={evt => this.setState({ password: evt.target.value })} />
+                                                    </Form.Group>
+                                                    <div className="row">
+                                                        <div className="col-sm-12 col-md-4 offset-md-4">
+                                                            <Button variant="primary" type="submit" className="btn btn-primary btn-user btn-block">
+                                                                Entrar
+                                                    </Button>
+                                                        </div>
+                                                    </div>
+                                                </Form>
+                                            </div>
+                                        </div>
+                                    </Row>
+                                </Card.Body>
+
+                                {(this.state.error != "") ? (
+                                    <Card.Footer>
+                                        <Alert variant="danger">{this.state.error}</Alert>
+                                    </Card.Footer>
+                                )
+                                    : ""}
+
+                            </Card>
+                            {this.state.loading ? (<Loader />) : ""}
                         </div>
-                    </Card.Header>
-                    <Card.Body>
-                        <Form onSubmit={this.handleLogin}>
-                            <Form.Group controlId="login">
-                                <Form.Label>Login</Form.Label>
-                                <Form.Control type="text" placeholder="Digite seu login" onChange={evt => this.setState({ user: evt.target.value })} />
-                            </Form.Group>
-
-                            <Form.Group controlId="senha">
-                                <Form.Label>Senha</Form.Label>
-                                <Form.Control type="password" placeholder="Digite sua senha" onChange={evt => this.setState({ password: evt.target.value })} />
-                            </Form.Group>
-                            <div className="row">
-                                <div className="col-sm-12 col-md-4 offset-md-4">
-                                    <Button variant="primary" type="submit" className="justify-content-center">
-                                        Entrar
-                                    </Button>
-                                </div>
-                            </div>
-                        </Form>
-                    </Card.Body>
-                    {(this.state.error != "") ? (
-                        <Card.Footer>
-                            <Alert variant="danger">{this.state.error}</Alert>
-                        </Card.Footer>
-                    )
-                        : ""}
-
-                </Card>
-                {this.state.loading ? (<Loader />) : ""}
+                    </Row>
+                </Container>
             </div>
         )
     }
