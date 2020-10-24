cf1e76801efdaed645620161c994914bb2b44894
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2020-10-24 16:46:56 -0300

    Atualiza o footer da página.
    [Ticket HIS009]

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,14 +10,14 @@ import Sidebar from './Sidebar.js';
 import Header from './Header.js';
 import Util from '../utils/Util.js';
 
-const PrivateRoute = ({component: Component, ...rest}) => (
+const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={props => (
         isAuthenticated() ? (
-            <Component {...props}/>
+            <Component {...props} />
         ) : (
-            <Redirect to={{pathname: '/login'}}/>
-        )
-    )}/>
+                <Redirect to={{ pathname: '/login' }} />
+            )
+    )} />
 )
 
 
@@ -31,44 +31,39 @@ const Content = () => {
                     <Sidebar />
                     <div id="content-wrapper" className="d-flex flex-column container-fluid">
                         {
-                            isAuthenticated() 
-                            ? (<Header userName={Util.decodeJWT().full_name}/>)
-                            : (<Header />)
-                        
+                            isAuthenticated()
+                                ? (<Header userName={Util.decodeJWT().full_name} />)
+                                : (<Header />)
+
                         }
-                        
+
                         <div id="content">
                             <Switch>
-                                <PrivateRoute exact path="/" component={()=>(<div></div>)} />
+                                <PrivateRoute exact path="/" component={() => (<div></div>)} />
 
                                 <PrivateRoute exact path={`/user/interacoes`} component={UserInteractions} />
-                            
-                                <PrivateRoute exact path={`/user/solicitacoes`} component={UserCreations}/ >
-                            
-                                <PrivateRoute exact path={`/servicos`} component={Servicos}/>
-                            
+
+                                <PrivateRoute exact path={`/user/solicitacoes`} component={UserCreations} />
+
+                                <PrivateRoute exact path={`/servicos`} component={Servicos} />
+
                                 <PrivateRoute exact path={`/servico/:id`} component={Servico} />
-                            
-                                <PrivateRoute exact path={`/404`} component={NotFound}/>
+
+                                <PrivateRoute exact path={`/404`} component={NotFound} />
                             </Switch>
                         </div >
                     </div>
                 </div>
             </main>
-            <footer className="py-4 bg-light mt-auto">
-                <div className="container-fluid">
-                    <div className="d-flex align-items-center justify-content-between small">
-                        <div className="text-muted">Copyright &copy; Your Website 2019</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                                &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Process Monitor {new Date().getFullYear()}</span>
                     </div>
                 </div>
             </footer>
         </div>
     )
 }
-    
+
 export default Content;
